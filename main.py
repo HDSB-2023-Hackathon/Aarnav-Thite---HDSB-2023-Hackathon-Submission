53babe4a98078199eefcfe21bce80d6bde8214db
EleKtr1X <aarnav.thite@gmail.com>
2023-03-04 13:19:15 -0500

    Add title input

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import sys
 import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
 
-class Flashcards(QtWidgets.QDialog):
+class Flashcards(QMainWindow):
   def __init__(self):
     super().__init__()
 
-class MainWindow(QtWidgets.QMainWindow):
-  def __init__(self):
-    super().__init__()
+    self.titleLabel = QLabel("Flashcard Set Title")
+    self.title = QLineEdit()
+    self.titleSubmit = QPushButton("Next")
 
-    self.button = QtWidgets.QPushButton("Add flashcard set")
-    self.text = QtWidgets.QLabel("You don't have any flashcard sets added yet.",
-                                  alignment=QtCore.Qt.AlignCenter)
-    
-    widget = QtWidgets.QWidget()
+    widget = QWidget()
     self.setCentralWidget(widget)
-    gridLayout = QtWidgets.QGridLayout(widget)
+    gridLayout = QGridLayout(widget)
+    gridLayout.addWidget(self.titleLabel, 0, 0)
+    gridLayout.addWidget(self.title, 0, 1)
+    gridLayout.addWidget(self.titleSubmit, 1, 0, 1, 2, Qt.AlignCenter)
+
+    self.titleSubmit.clicked.connect(self.acceptTitle)
+
+  def acceptTitle(self):
+    title = self.title.text()
+    if not title:
+      self.title.setStyleSheet("border: 1px solid red")
+    else:
+      self.title.setStyleSheet("")
+      print(title)
+
+class MainWindow(QMainWindow):
+  def __init__(self):
+    super().__init__()
+
+    self.button = QPushButton("Add flashcard set")
+    self.text = QLabel("You don't have any flashcard sets added yet.",
+                                  alignment=Qt.AlignCenter)
+    
+    widget = QWidget()
+    self.setCentralWidget(widget)
+    gridLayout = QGridLayout(widget)
     gridLayout.addWidget(self.text, 0, 0)
     gridLayout.addWidget(self.button, 1, 0)
 
@@ -24,10 +47,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
   def openFlashcards(self):
     self.flashcards = Flashcards()
+    self.flashcards.resize(300, 100)
     self.flashcards.show()
 
 if __name__ == "__main__":
-  app = QtWidgets.QApplication([])
+  app = QApplication([])
 
   window = MainWindow()
   window.resize(800, 600)
