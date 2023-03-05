62cb41c9ecc039621a18a04e4ad34e3e3bc554c4
EleKtr1X <aarnav.thite@gmail.com>
2023-03-04 23:08:05 -0500

    create "edit" and "test" buttons for every deck

--- package/mainwindow.py
+++ package/mainwindow.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import Qt
 from package.flashcards import Flashcards
 
 with open('flashcard.json', 'r') as f:
-      flashcardList = json.load(f)
+  flashcardList = json.load(f)
 
 class MainWindow(QMainWindow):
   def __init__(self):
@@ -14,22 +14,36 @@ class MainWindow(QMainWindow):
     self.text = QLabel("You don't have any flashcard sets added yet.",
                                   alignment=Qt.AlignCenter)
     
-    self.selector = QListWidget()
-    for item in flashcardList:
-      self.selector.addItem(QListWidgetItem(item))
-    self.selector.setCurrentRow(0)
-    
     widget = QWidget()
     self.setCentralWidget(widget)
     gridLayout = QGridLayout(widget)
+    titles = flashcardList.keys()
 
+    self.deck = QGroupBox("Decks")
+    self.deckVBox = QVBoxLayout()
+    self.deckVBox.setAlignment(Qt.AlignTop)
+    self.deck.setLayout(self.deckVBox)
+    
     if len(flashcardList) == 0:
-      gridLayout.addWidget(self.text, 0, 0)
+      self.deckVBox.addWidget(self.text)
     else:
-      gridLayout.addWidget(self.selector, 0, 0)
+      i = 0
+      for item in titles:
+        grid = QGridLayout()
+        editButton = QPushButton("Edit")
+        testButton = QPushButton("Test")
+        label = QLabel(item)
+        label.setStyleSheet("font-weight: bold")
+        grid.addWidget(label, i, 0, 1, 2, Qt.AlignTop)
+        grid.addWidget(editButton, i, 2, Qt.AlignTop)
+        grid.addWidget(testButton, i, 3, Qt.AlignTop)
+        self.deckVBox.addLayout(grid)
+        print(self.deck)
+        i += 1
 
+    gridLayout.addWidget(self.deck, 0, 0)
     gridLayout.addWidget(self.button, 1, 0)
-
+    self.setLayout(gridLayout)
     self.button.clicked.connect(self.openFlashcards)
 
   def openFlashcards(self):
