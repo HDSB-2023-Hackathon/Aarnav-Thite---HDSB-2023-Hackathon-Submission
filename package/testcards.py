81ec02816e94008f87cb3a61faf3768565cef70c
EleKtr1X <aarnav.thite@gmail.com>
2023-03-05 11:51:20 -0500

    keep testcards window open

--- package/testcards.py
+++ package/testcards.py
@@ -1,11 +1,12 @@
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 
-class TestCards(QWidget):
+class TestCards(QMainWindow):
   def __init__(self, title):
     super().__init__()
 
-    self.central_widget = QStackedWidget()
-    self.setCentralWidget(self.central_widget)
+    # self.central_widget = QStackedWidget()
+    # self.setCentralWidget(self.central_widget)
 
     self.setWindowTitle("testing")
+    print("testcards", title)
